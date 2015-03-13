d2f04f8b5e7ecc1e944f34bc8affd906281fb5ca
christianmed <medinachristian@outlook.com>
2015-03-13 20:06:35 +0000

    Ahora año debe ser >= 1

--- 2015/012-bisiesto.pl
+++ 2015/012-bisiesto.pl
@@ -13,7 +13,7 @@
 # por 100, o si es divisible por 400. Escribe un      #
 # programa que lea un año y devuelva si es bisiesto   #
 # o no.                                               #
-# Se comprueba que el numero sea >= 0                 #
+# Se comprueba que el numero sea >= 1                 #
 #######################################################
 
 #Limpiamos la pantalla
@@ -29,7 +29,7 @@ print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 do{
         print "Año a consultar: ";
         chomp($Year=<STDIN>);
-} while($Year<0);
+} while($Year<1);
 
 #Comprobacion
 ((!($Year%4) && $Year%100) || !($Year%400)) ? print "\nEs Bisiesto!" : print "\nNo es Bisiesto!";
