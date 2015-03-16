e28927818f189dea36e16feb171f76e681d49911
Christian Medina <medinachristian1984@gmail.com>
2015-03-15 19:46:22 -0430

    Initial commit

---
+++ 2015/018-pvp.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+
+########################################################
+#Nombre:    
+#Fecha:     00/00/2015                                 #
+#Versión:   0.1                                        #
+#By:        Christian Medina                           #
+########################################################
+
+#######################################################
+#                     Descripción                     #
+#                                                     #
+# El dueño de una papelería desea un programa que le  #
+# indique el precio de venta de un artículo dado. El  #
+# precio se calcula de acuerdo con la siguiente       #
+# fórmula: PVP = precio_coste + ganancia. Donde la    #
+# ganancia será:                                      #
+#                                                     #
+#  • El 15% si el precio de coste es inferior Bs. 3.  #
+#  • 50 céntimos si el precio de coste está entre     #
+#  	 Bs. 3 y Bs. 6.                                   #
+#  • El 25% si el precio de coste supera los Bs. 6.   #
+#######################################################
+
+#Limpiar pantalla
+print `clear`;
+
+#Definicion de variables
+
+
+#Mensaje de Bienvenida
+print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+print "\t\tBienvenidos\n";
+print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+print "==========-----------------------==========";
+print "\n VERIFIQUE EL MONTO A PAGAR POR SU LLAMADA\n";
+print "==========-----------------------==========\n\n";
+
+#Solicitamos el ingreso del numero por teclado
+
+
+
+
+
+
+
+
+#Mensaje de despedida
+print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+print "\t\t  Adios!!\n";
+print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
