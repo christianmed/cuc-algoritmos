75de007f0c6d5281b49b3b87ae1811a2b1a2b346
Christian Medina <medinachristian1984@gmail.com>
2015-03-16 23:06:32 -0430

    Programa terminado

--- 2015/018-pvp.pl
+++ 2015/018-pvp.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 
 ########################################################
-#Nombre:    
-#Fecha:     00/00/2015                                 #
+#Nombre:    018-pvp.pl                                 #
+#Fecha:     16/03/2015                                 #
 #Versión:   0.1                                        #
 #By:        Christian Medina                           #
 ########################################################
@@ -22,31 +22,44 @@
 #  • El 25% si el precio de coste supera los Bs. 6.   #
 #######################################################
 
-#Limpiar pantalla
-print `clear`;
+do{
+	#Limpiar pantalla
+	print `clear`;
 
-#Definicion de variables
+	#Definicion de variables
+	my $pvp=0;
+	my $pc=0;
+	my $gn=0;
 
+	#Mensaje de Bienvenida
+	print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+	print "\t\tBienvenidos\n";
+	print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
-#Mensaje de Bienvenida
-print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\tBienvenidos\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+	print "==========-----------------------==========";
+	print "\n    PRECIO DE VENTA DE LOS ARTICULOS\n";
+	print "==========-----------------------==========\n\n";
 
-print "==========-----------------------==========";
-print "\n VERIFIQUE EL MONTO A PAGAR POR SU LLAMADA\n";
-print "==========-----------------------==========\n\n";
+	#Solicitamos el ingreso del numero por teclado
+	print "Indique el Precio de Coste: ";
+	chomp($pc=<STDIN>);
 
-#Solicitamos el ingreso del numero por teclado
+	if($pc>=0 && $pc<3){
+		$pvp=$pc+($pc*0.15);
+	}
+	elsif($pc>=3 && $pc<=6){
+		$pvp=$pc+0.5;
+	}
+	else{
+		$pvp=$pc+($pc*0.25);
+	}
+	print "\nEl Precio de venta es de Bs. $pvp\n";
 
-
-
-
-
-
-
+	print "\nDesea calcular el Precio de otro Articulo (s/n): ";
+	chomp($op=<STDIN>);
+}while($op eq "s" || $op eq "S");
 
 #Mensaje de despedida
-print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
 print "\t\t  Adios!!\n";
 print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
