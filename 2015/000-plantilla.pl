0ea74156e01c5c6cbf679aac9cd6f7cf520cc342
Christian Medina <medinachristian1984@gmail.com>
2015-03-22 11:05:08 -0430

    se agregan funciones con mensajes de bienvenida y despedida

--- 2015/000-plantilla.pl
+++ 2015/000-plantilla.pl
@@ -13,23 +13,37 @@
 #
 #######################################################
 
+#Funcion Mensaje de Bienvenida
+sub MenBienvenida{
+    print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    print "\t\tBienvenidos\n";
+    print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+    
+    print "==========-----------------------==========";
+    print "\n \n";
+    print "==========-----------------------==========\n\n";
+}
+
+#Funcion Mensaje de despedida
+sub MenDespedida{
+    print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
+    print "\t\t  Adios!!\n";
+    print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+}
+
 #Limpiar pantalla
 print `clear`;
+
+#Llamamos a la funcion con el mensaje de bienvenida
+MenBienvenida();
 
 #Definicion de variables
 
 
-#Mensaje de Bienvenida
-print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\tBienvenidos\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
 #Solicitamos el ingreso del numero por teclado
 
 
 
-
-#Mensaje de despedida
-print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\t  Adios!!\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+#Llamamos a la funcion con el mensaje de despedida
+MenDespedida();
