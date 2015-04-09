555586c3122cfa50c8f13659f303332597a2f4df
Christian Medina <medinachristian@outlook.com>
2015-04-09 18:16:08 +0000

    Agragedo modulo Scalar::Util para comprobrar valor numéricos

--- 2015/002-su-res-mul.pl
+++ 2015/002-su-res-mul.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 
 #######################################################
-#Nombre:    002-su-res-mul.pl                         #
-#Fecha:     05/03/2015                                #
-#Versión:   0.1                                       #
-#By:        Christian Medina                          #
+#Nombre:        002-su-res-mul.pl                     #
+#Fecha:         05/03/2015                            #
+#Versión:       0.1                                   #
+#By:            Christian Medina                      #
+#Modificado:    09/04/2015                            #
 #######################################################
 
 #######################################################
@@ -16,20 +17,71 @@
 #muestran los resultados por pantalla.                #
 #######################################################
 
-#Mensaje de Bienvenida
-print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\tBIENVENIDOS\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+use Scalar::Util 'looks_like_number';
+    
+#Función Mensaje de Bienvenida
+sub MenBienvenida{
+    #Limpiamos la pantalla
+    print `clear`;
+    print "\n==========-----------------------==========\n";
+    print "\t\tBIENVENIDOS\n";
+    print "==========-----------------------==========\n";
+    print "==========-----------------------==========\n";
+    print "   SUMA, RESTA Y PRODUCTO DE DOS NUMEROS\n";
+    print "==========-----------------------==========\n\n";
+}
 
-#Solicitamos el ingreso del numero por teclado
-print "Introduzca un Numero: ";
-chomp($numA=<STDIN>);
+#Funcion Mensaje de despedida
+sub MenDespedida{
+    print "\n\n==========-----------------------==========\n";
+    print "\t\t  ADIOS!!\n";
+    print "==========-----------------------==========\n\n";
+}
 
-#verificamos que el numero sea positivo
+#Funcíon con emsnaje de error
+sub Error{
+    print "\nERROR ---> Pulse una tecla para continuar...";
+    <STDIN>;
+}
+
+#Función para solicitar un valor
+sub IntroNum{
+    print "Introduzca un Numero: ";
+    chomp(my $num=<STDIN>);
+    return $num;
+}
+
+do{
+    #Llamamos a la función con el mensaje de bienvenida
+    MenBienvenida();
+    
+    print "VALOR DE A\n\n";
+    #Solicitamos el ingreso del número por teclado
+    $numA = IntroNum();
+    
+    #Comprobamos que se introduzca un número
+    unless(looks_like_number($numA)){
+        Error();
+    }
+
+}while(!looks_like_number($numA));
+
+#verificamos que el número sea positivo
 if ($numA>-1) {
-    #Solicitamos el ingreso del segundo numero por teclado
-    print "Introduzca otro Numero: ";
-    chomp($numB=<STDIN>);
+    do{
+        #Llamamos a la función con el mensaje de bienvenida
+        MenBienvenida();
+        
+        print "VALOR DE B\n\n";
+        #Solicitamos el ingreso del segundo número por teclado
+        $numB = IntroNum();
+        
+        #Comprobamos que se introduzca un número
+        unless(looks_like_number($numB)){
+        Error();
+    }
+        
+    }while(!looks_like_number($numB));
     
     #Realizamos las operaciones aritméticas
     $suma =      $numA + $numB;
@@ -41,12 +93,10 @@ if ($numA>-1) {
     print "Resta: $numA - $numB = $resta\n";
     print "Producto: $numA * $numB = $producto";
 }
-#Si el numero es negativo se muestra el siguiente mensaje
+#Si el número es negativo se muestra el siguiente mensaje
 else {
     print "\nEl numero que introdujo es Negativo!";
 }
 
-#Mensaje de despedida
-print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\t  Adios!!\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+#Llamamos a la funcion con el mensaje de despedida
+MenDespedida();
