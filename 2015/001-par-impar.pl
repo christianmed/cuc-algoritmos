7add6b7259a40452575c763c82e09609d84b02d9
Christian Medina <medinachristian@outlook.com>
2015-04-09 17:42:19 +0000

    agregado modulo Scalar::Util para comprobar valores numéricos, agragdo mensaje de error en caso de que se introduzca un valor no numérico

--- 2015/001-par-impar.pl
+++ 2015/001-par-impar.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 
 #######################################################
-#Nombre:    001-par-impar.pl                          #
-#Fecha:     05/03/2015                                #
-#Versión:   0.1                                       #
-#By:        Christian Medina                          #
+#Nombre:        001-par-impar.pl                      #
+#Fecha:         05/03/2015                            #
+#Versión:       0.1                                   #
+#By:            Christian Medina                      #
+#Modificado:    09/04/2015 - v0.2                     #
 #######################################################
 
 #######################################################
@@ -14,25 +15,63 @@
 #determina si es par o impar                          #
 #######################################################
 
-#Mensaje de Bienvenida
-print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\tBIENVENIDOS\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+use Scalar::Util 'looks_like_number';
 
-#Solicitamos el ingreso del numero por teclado
-print "Introduzca un Numero Entero: ";
-chomp($num=<STDIN>);
+#Función Mensaje de Bienvenida
+sub MenBienvenida{
+    #Limpiamos la pantalla
+    print `clear`;
+    print "\n==========-----------------------==========\n";
+    print "\t\tBIENVENIDOS\n";
+    print "==========-----------------------==========\n";
+    print "==========-----------------------==========\n";
+    print "   DETERMINAR SU UN NUMERO ES PAR O IMPAR\n";
+    print "==========-----------------------==========\n\n";
+}
 
-#verificamos si el numero introducido es = 0
+#Funcion Mensaje de despedida
+sub MenDespedida{
+    print "\n\n==========-----------------------==========\n";
+    print "\t\t  ADIOS!!\n";
+    print "==========-----------------------==========\n\n";
+}
+
+sub Error{
+    print "\nERROR ---> Pulse una tecla para continuar...";
+    <STDIN>;
+}
+
+##########==================CUERPO DEL PROGRAMA
+
+do{
+    #Mostramos el mensaje de bienvenida
+    MenBienvenida();
+    
+    #Solicitamos el ingreso del número por teclado
+    print "Introduzca un Numero Entero Positivo: ";
+    chomp($num=<STDIN>);
+    
+    #Comprobamos que se haya introducido un número
+    if(!looks_like_number($num)){
+        #Se muestra un mensaje de error
+        Error();
+    }
+    #Comprobamos que el número sea >= 0
+    elsif($num < 0){
+        #Se muestra un mensaje de error
+        Error();
+    }
+
+}while($num < 0 || (!looks_like_number($num)));
+
+#verificamos si el número introducido es = 0
 if ($num==0){
-    print "\nEl numero que introdujo es 0\n";
+    print "\nEl 0 es nu numero neutro";
 }
-#Si el numero es diferente de 0 comprobamos si es par o impar y nomstramos mensaje por pantalla
+#Si el número es diferente de 0 comprobamos si es par o impar y mosstramos mensaje por pantalla
 else {
-    ($num%2)==0 ? print "\nEl numero que introdujo es Par\n" : print "El numero que introdujo es Impar\n;"
+    ($num%2)==0 ? print "\nEl numero que introdujo es Par" : print "\nEl numero que introdujo es Impar";
 }
 
-#Mensaje de despedida
-print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\t  Adios!!\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+#Llamamos a la funcion con el mensaje de despedida
+MenDespedida();
