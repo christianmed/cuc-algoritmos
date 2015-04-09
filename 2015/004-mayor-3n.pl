4b9d52e5fc7e46312f4dfae72bf4ffdbe1a17385
Christian Medina <medinachristian@outlook.com>
2015-04-09 20:18:52 +0000

    v0.2 - agregado módulo Scalar::Util para comprobar valores numéricos, se mensaje de bienvenida, despedida y error en funciones

--- 2015/004-mayor-3n.pl
+++ 2015/004-mayor-3n.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 
 #######################################################
-#Nombre:    004-mayor-3n.pl                           #
-#Fecha:     05/03/2015                                #
-#Versión:   0.1                                       #
-#By:        Christian Medina                          #
+#Nombre:        004-mayor-3n.pl                       #
+#Fecha:         05/03/2015                            #
+#Versión:       0.1                                   #
+#By:            Christian Medina                      #
+#Modificado:    09/04/2015 - v0.2                     #
 #######################################################
 
 #######################################################
@@ -13,49 +14,84 @@
 #programa que calcula el mayor de 3 numeros introdu-  #
 #cidos por teclado y muestra el resultado por         #
 #pantalla.                                            #
-#                                                     #
-#  **Se comprueba que los números sean diferentes**   #
 #######################################################
 
-#Mensaje de Bienvenida
-print "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\tBIENVENIDOS\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+use Scalar::Util 'looks_like_number';
 
-#Solicitamos el ingreso de los números por teclado
-print "Introduzca el Valor de A: ";
-chomp($numA=<STDIN>);
-#Comprobamos que 2do número sea diferente del 1er número
-do {
-    print "Introduzca el Valor de B: ";
-    chomp($numB=<STDIN>);
+#Función Mensaje de Bienvenida
+sub MenBienvenida{
+    #Limpiamos la pantalla
+    print `clear`;
+    print "\n==========-----------------------==========\n";
+    print "\t\tBIENVENIDOS\n";
+    print "==========-----------------------==========\n";
+    print "==========-----------------------==========\n";
+    print "     DETERMINAR MAYOR DE TRES NUMEROS\n";
+    print "==========-----------------------==========\n\n";
 }
-while ($numA==$numB);
-#Comprobamos que 3er número sea diferente del 1er y 2do número
-do {
-    print "Introduzca el Valor de C: ";
-    chomp($numC=<STDIN>);
+
+#Funcion Mensaje de despedida
+sub MenDespedida{
+    print "\n\n==========-----------------------==========\n";
+    print "\t\t  ADIOS!!\n";
+    print "==========-----------------------==========\n\n";
 }
-while ($numC==$numA || $numC==$numB);
+
+#Función para solicita un número por teclado
+sub IntroNum{
+    
+    my $letra = shift;
+    
+    #Llamamos a la función con el mensaje de bienvenida
+    MenBienvenida();
+    
+    print "VALOR DE $letra\n\nIntroduzca un Numero: ";
+    chomp(my $num = <STDIN>);
+    
+    #Si no se introduce un número entonces
+    unless(looks_like_number($num)){
+        #Se muestra un mensaje de error
+        Error();
+        #Se solicita la introduccioón de otro valor
+        $num = IntroNum();
+    }
+    return $num;
+}
+
+sub Error{
+    print "\nERROR ---> Pulse una tecla para continuar...";
+    <STDIN>;
+}
+
+###############################################
+#####          CUERPO DEL PROGRAMA          ###
+###############################################
+
+#Solicitamos el valor de A y pasamos como parámetro la letra A
+$numA = IntroNum('A');
+
+#Solicitamos el valor de B y pasamos como parámetro la letra B
+$numB = IntroNum('B');
+
+#Solicitamos el valor de C y pasamos como parámetro la letra C
+$numC = IntroNum('C');
+
+#Llamamos a la función con el mensaje de bienvenida
+MenBienvenida();
 
 #Comprobación del mayor de los números
-if ($numA > $numB && $numA > $numC) {
-    #Se guarda el valor en la variable $mayor
-    $mayor = $numA;
-}
-elsif ($numB > $numA && $numB > $numC){
-    #Se guarda el valor en la variable $mayor
-    $mayor = $numB;
-}
-else {
-    #Se guarda el valor en la variable $mayor
-    $mayor = $numC;
-}
 
-#Se muestra por pantalla el valor de $mayor
-print "\nNumero Mayor: $mayor";
+#Si los tres números son iguales mostramos el siguiente mensaje
+($numA == $numB && $numA == $numC && $numB == $numC) and print "Los tres numeros son iguales!";
 
-#Mensaje de despedida
-print "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n";
-print "\t\t  Adios!!\n";
-print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+#Comprobamos Si A es mayor y mostramos su valor
+($numA > $numB && $numA > $numC) and print "$numA es el Mayor de los Numeros!";
+
+#Comprobamos Si B es mayor y mostramos su valor
+($numB > $numA && $numB > $numC) and print "$numB es el Mayor de los Numeros!";
+
+#Comprobamos Si C es mayor y mostramos su valor
+($numC > $numA && $numC > $numB) and print "$numC es el Mayor de los Numeros!";
+
+#Llamamos a la funcion con el mensaje de despedida
+MenDespedida();
